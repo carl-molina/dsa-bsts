069cba40c4d3a057d9ca5276273008a716451e94
Veronica Ni <vni1294@gmail.com>
2024-02-29 18:47:35 -0500

    Working on bfs

--- bsts/bfs/bfs.ts
+++ bsts/bfs/bfs.ts
@@ -1,15 +1,23 @@
 import { BNodeNum } from "../common/bst";
+import { Queue } from "../common/queue";
 
 
 /** bfs(): Traverse the BST using BFS.
  * Returns an array of visited nodes. */
 
 function bfs(node: BNodeNum | null): number[] {
+  const nums: number[] = [];
+  const toVisit = new Queue([node]);
+  if (!node) return [];
 
-  // TODO: implement fn here
+  while (!toVisit.isEmpty()) {
+    let curr = toVisit.dequeue();
+    if (curr) nums.push(curr.val);
+  }
+  toVisit.enqueue(node.left);
+  toVisit.enqueue(node.right);
 
-
-  return [42];
+  return nums;
 }
 
 export { bfs };
