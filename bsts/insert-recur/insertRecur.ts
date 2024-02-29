287084f9c6686589b5d2628e32a92cfa9435874a
Carl Molina <carlandrew.molina@gmail.com>
2024-02-29 14:22:27 -0800

    added console.logs

--- bsts/insert-recur/insertRecur.ts
+++ bsts/insert-recur/insertRecur.ts
@@ -8,15 +8,18 @@ function insertRecur(bst: BSTNum, val: number): void {
 
   if (bst.root === null) {
     bst.root = newNode;
+    console.log('This is newNode which is now root:', bst.root);
     return;
   }
 
   if (val < bst.root.val) {
     insertRecur(new BSTNum(bst.root.left), val);
     bst.root.left = newNode;
+    console.log('Original root after left:', bst.root);
     return;
   } else if (val > bst.root.val) {
     insertRecur(new BSTNum(bst.root.right), val);
+    console.log('Original root after right:', bst.root);
     bst.root.right = newNode;
     return;
   }
