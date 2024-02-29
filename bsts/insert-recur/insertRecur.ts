f02ed2fab88c311b6eda5d4263058e1ac43e6ffc
Veronica Ni <vni1294@gmail.com>
2024-02-29 15:20:24 -0500

    Working on insertRecursive

--- bsts/insert-recur/insertRecur.ts
+++ bsts/insert-recur/insertRecur.ts
@@ -1,9 +1,21 @@
-import { BSTNum } from "../common/bst";
+import { BSTNum, BNode } from "../common/bst";
 
 /** insertRecursively(val): Insert a new node into the BST with value val.
  * Uses recursion. */
 
 function insertRecursively(bst: BSTNum, val: number): void {
+  const newNode = new BNode(val);
+  
+  if (bst.root === null) {
+    bst.root = newNode;
+  }
+
+  if (val < bst.root.val) {
+    newNode.right = bst.root;
+  }
+
+  //else if val > bst.root.val
+  //TODO: 
 }
 
 export { insertRecursively };
